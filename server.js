1d0abe754a67e9795898b26aa7020696f18d1599
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2023-09-23 15:11:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const cors = require('cors');
 dotenv.config();
 
 const newSchema = new mongoose.Schema({
-// Schema definition
 _id: ObjectId,
 end_year: String,
 intensity: Number,
@@ -36,14 +35,11 @@ const User = mongoose.model('User', newSchema);
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5500', // Replace with your frontend's domain
+  origin: 'http://localhost:5500', 
 }));
 app.use(bodyparser.json());
 app.use(express.json());
 
-// app.get('/', (req, resp) => {
-// resp.send('welcome');
-// });
 
 mongoose
 .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -54,10 +50,10 @@ console.log('connected to db');
 
 app.get('/api/data',  async(req, res) => {
 try {
-// Use the User model to access the collection
+
 
  const data =  await User.find({})
-//  .select('-_id intensity').limit(10)
+
  const intensity = data.map(item => item.intensity);
  const country=data.map(item=>item.country);
 const year = data.map(item=>item.start_year)
@@ -68,20 +64,13 @@ const year = data.map(item=>item.start_year)
   
   res.json({intensity, likelihood,relevance,year,country,topics});
   
-
-  
-// res.json(data);
 } catch (error) {
 console.error('Error:', error);
 res.status(500).json({ error: 'Error fetching data' });
 }
 });
 
-// const port = process.env.PORT || '3000' ;
 
-// app.listen(port, () => {
-// console.log('connection done');
-// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
